c2667b747e20fc8160169908f59b8cb8a91f9f36
Kamesuta <kamesuta@gmail.com>
2024-08-12 12:07:56 +0900

    daemon version and url to .env

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,12 @@
 import 'dotenv/config';
 import assert from 'assert';
 
+assert(process.env.DISCORD_TOKEN, 'DISCORD_TOKEN is not set');
+/**
+ * The Discord bot token.
+ */
+export const DISCORD_TOKEN: string = process.env.DISCORD_TOKEN;
+
 /**
  * The port number for the server.
  * If the `PORT` environment variable is set, it will be used as the port number.
@@ -8,8 +14,18 @@ import assert from 'assert';
  */
 export const PORT: number = parseInt(process.env.PORT || '3000');
 
-assert(process.env.DISCORD_TOKEN, 'DISCORD_TOKEN is not set');
+assert(
+  process.env.DAEMON_REQUIRED_VERSION,
+  'DAEMON_REQUIRED_VERSION is not set',
+);
 /**
- * The Discord bot token.
+ * Required version of the daemon client.
  */
-export const DISCORD_TOKEN: string = process.env.DISCORD_TOKEN;
+export const DAEMON_REQUIRED_VERSION: string =
+  process.env.DAEMON_REQUIRED_VERSION;
+
+assert(process.env.DAEMON_DOWNLOAD_URL, 'DAEMON_DOWNLOAD_URL is not set');
+/**
+ * URL to guide downloading when the daemon client is outdated.
+ */
+export const DAEMON_DOWNLOAD_URL: string = process.env.DAEMON_DOWNLOAD_URL;
